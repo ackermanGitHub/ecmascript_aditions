6c968622735adef95e78b90b83acd1e7f89ec619
julio <julio.sergio2709@gmail.com>
2022-05-18 03:18:06 -0400

    Second Module ECMAScript7

---
+++ src/es7/index.js
@@ -0,0 +1,12 @@
+let numbers = [1,2,3,7,8];
+let value = 9;
+if (numbers.includes(value)){
+    console.log("Si se encuentra el valor " + value);
+} else {
+    console.log("No se encuentra el valor " + value);
+}
+
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+console.log(result);
